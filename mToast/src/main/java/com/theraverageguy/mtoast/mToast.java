16b159457e9c0f23c0ffb6b72662807c946983a6
hgnis <gangwar.jar@gmail.com>
2021-01-28 16:03:14 +0530

    minor changes

--- mToast/src/main/java/com/theraverageguy/mtoast/mToast.java
+++ mToast/src/main/java/com/theraverageguy/mtoast/mToast.java
@@ -7,7 +7,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class mToast {
-    public static void errorShow(Context context) {
+
+    public static void error(Context context) {
         Toast toast = Toast.makeText(context, "Something went wrong !!", Toast.LENGTH_LONG);
         View view = toast.getView();
         view.getBackground().setColorFilter(context.getResources().getColor(R.color.colorRed), PorterDuff.Mode.SRC_IN);
@@ -17,7 +18,7 @@ public class mToast {
         toast.show();
     }
 
-    public static void errorMessageShow(Context context, String Message) {
+    public static void errorMessage(Context context, String Message) {
         Toast toast = Toast.makeText(context, Message, Toast.LENGTH_LONG);
         View view = toast.getView();
         view.getBackground().setColorFilter(context.getResources().getColor(R.color.colorRed), PorterDuff.Mode.SRC_IN);
@@ -27,7 +28,7 @@ public class mToast {
         toast.show();
     }
 
-    public static void toastShow(Context context, String Message) {
+    public static void Show(Context context, String Message) {
         Toast toast = Toast.makeText(context, Message, Toast.LENGTH_LONG);
         View view = toast.getView();
         TextView text = view.findViewById(android.R.id.message);
@@ -36,8 +37,18 @@ public class mToast {
         toast.show();
     }
 
-    public static void successShow(Context context) {
-        Toast toast = Toast.makeText(context, "Done", Toast.LENGTH_LONG);
+    public static void success(Context context) {
+        Toast toast = Toast.makeText(context, "Success ", Toast.LENGTH_LONG);
+        View view = toast.getView();
+        view.getBackground().setColorFilter(context.getResources().getColor(R.color.colorLightGreen), PorterDuff.Mode.SRC_IN);
+        TextView text = view.findViewById(android.R.id.message);
+        text.getTextSize();
+        text.setTextColor(context.getResources().getColor(R.color.colorWhite));
+        toast.show();
+    }
+
+    public static void successMessage(Context context, String Message) {
+        Toast toast = Toast.makeText(context, Message, Toast.LENGTH_LONG);
         View view = toast.getView();
         view.getBackground().setColorFilter(context.getResources().getColor(R.color.colorLightGreen), PorterDuff.Mode.SRC_IN);
         TextView text = view.findViewById(android.R.id.message);
@@ -50,7 +61,7 @@ public class mToast {
         Toast.makeText(context, "I am alive !! ", Toast.LENGTH_LONG).show();
     }
 
-    public static void showToast(Context context, String message, int BackgroundColor, int textColor) {
+    public static void custom(Context context, String message, int BackgroundColor, int textColor) {
         Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
         View view = toast.getView();
         view.getBackground().setColorFilter(BackgroundColor, PorterDuff.Mode.SRC_IN);
